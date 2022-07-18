8ffa340b004977a37ae2f2c2c4af36128697d918
lucns <lucns2906@gmail.com>
2022-07-17 23:08:59 -0300

    Delete ATMega1284P/scripts directory

--- ATMega1284P/scripts/1Mhz_fuses.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title AvrDude Command Window
-@echo.
-@echo. trying to connect to device...
-avrdude -c usbasp -p m1284p -U lfuse:w:0x62:m -U hfuse:w:0xde:m -U efuse:w:0xff:m -U lock:w:0x3f:m
-pause
